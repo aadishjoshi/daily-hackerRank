ff1957ed04fc95d3991350d98b1351a37ccfe8ee
aadishjoshi <emailaadish@gmail.com>
2018-10-18 19:40:39 -0500

    save the prisoner problem E

---
+++ codes/saveThePrisoner.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int saveThePrisoner(int n, int m, int s){
+        int a = s+m-1;   
+        if(a>n){
+            if(a%n==0){
+                return n;
+            }
+            return a%n;
+        }
+      
+         return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int m = in.nextInt();
+            int s = in.nextInt();
+            int result = saveThePrisoner(n, m, s);
+            System.out.println(result);
+        }
+    }
+}
+
